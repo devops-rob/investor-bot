5295be782b802ad6c7eab63ce07ee1186fd369d7
Robert Barnes <robert.raybarnes@gmail.com>
2021-10-26 23:50:34 +0100

    adding more tests

--- coinbase/crypto-selecter_test.go
+++ coinbase/crypto-selecter_test.go
@@ -2,9 +2,11 @@ package coinbase
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"testing"
 	"twitch-investo-bot/mocks"
 )
@@ -45,4 +47,91 @@ func TestProductBookFail(t *testing.T) {
 		t.Error("TestProductBookFail failed.")
 		return
 	}
+}
+
+func TestOnlineFilter(t *testing.T) {
+	coins := `
+[
+  {
+    "id": "BAT-USDC",
+    "base_currency": "BAT",
+    "quote_currency": "USDC",
+    "base_min_size": "1",
+    "base_max_size": "300000",
+    "quote_increment": "0.000001",
+    "base_increment": "0.000001",
+    "display_name": "BAT/USDC",
+    "min_market_funds": "1",
+    "max_market_funds": "100000",
+    "margin_enabled": false,
+    "fx_stablecoin": false,
+    "max_slippage_percentage": "0.10000000",
+    "post_only": false,
+    "limit_only": false,
+    "cancel_only": false,
+    "trading_disabled": false,
+    "status": "online",
+    "status_message": "",
+    "auction_mode": false
+  }
+]`
+
+	cryptoAssets := make([]Product,0)
+	err := json.Unmarshal([]byte(coins), &cryptoAssets)
+	if err != nil {
+		t.Error("TestOnlineFilter failed. Unable to unmarshal json response")
+		return
+	}
+
+	for _, v := range cryptoAssets {
+		if v.Status != "online" {
+			t.Error("TestOnlineFilter failed. Status should be online")
+			return
+		}
+	}
+}
+
+func TestCurrencyFilter(t *testing.T) {
+	coins := `
+[
+  {
+    "id": "BAT-USDC",
+    "base_currency": "BAT",
+    "quote_currency": "USDC",
+    "base_min_size": "1",
+    "base_max_size": "300000",
+    "quote_increment": "0.000001",
+    "base_increment": "0.000001",
+    "display_name": "BAT/USDC",
+    "min_market_funds": "1",
+    "max_market_funds": "100000",
+    "margin_enabled": false,
+    "fx_stablecoin": false,
+    "max_slippage_percentage": "0.10000000",
+    "post_only": false,
+    "limit_only": false,
+    "cancel_only": false,
+    "trading_disabled": false,
+    "status": "online",
+    "status_message": "",
+    "auction_mode": false
+  }
+]`
+
+	cryptoAssets := make([]Product,0)
+	err := json.Unmarshal([]byte(coins), &cryptoAssets)
+	if err != nil {
+		t.Error("TestOnlineFilter failed. Unable to unmarshal json response")
+		return
+	}
+
+	for _, v := range cryptoAssets {
+		if !strings.Contains(v.QuoteCurrency, "USD") {
+			t.Error("TestOnlineFilter failed. Base currency should contain USD")
+			return
+		}
+	}
+}
+func TestCryptoPicker(t *testing.T) {
+
 }
